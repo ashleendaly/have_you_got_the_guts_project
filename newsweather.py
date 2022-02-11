e5f50c8441ad2a467b2aa81aae01a511cc9935a6
Shug1876 <2549100C@student.gla.ac.uk>
2022-02-11 20:42:10 +0000

    more on weather function

--- newsweather.py
+++ newsweather.py
@@ -1,14 +1,23 @@
 #import requests
-#from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
-def weather(country):
-    output1 = "Weather in " + country
-    pass
+def weather(country, uk=""):
+    country = country.title()
+    print("Weather in " + country)
+    country = country.lower()
+    if len(uk)!=0:
+        return "uk"
+    else:
+        if country=="usa" or country=="united states" or country == "united states of america":
+            country = "united_states_of_america"
+        url = "https://www.metoffice.gov.uk/weather/world/"+ country +"/list"
+        print(url)
+    return "passed"
 
 def news(country):
     output1 = "Recent news in " + country
 
-    pass
-
+    return output1
+weather("United States of America")
 search="Weather in the New York"
 URL ="https://openweathermap.org/city/5128581"
