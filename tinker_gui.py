e35cb27d7602ba0b10a6459c4267362aa356ff2b
2552805d <2552805d@student.gla.ac.uk>
2022-02-11 22:19:01 +0000

    added confirmation label to Add Colleague (needs to be finished)

--- tinker_gui.py
+++ tinker_gui.py
@@ -5,7 +5,7 @@ from newsweather import news, weather
 colleagues = []
 
 
-def add_colleague_to_dict(name, city):
+def add_colleague_to_dict():
     pass
 
 
@@ -27,19 +27,22 @@ add_colleague_label = Label(left_frame, text="Add Colleague").grid(row=0, column
 city_label = Label(left_frame, text="City:")
 city_label.grid(row=1, column=0, padx=5, pady=5)
 
-city_text = StringVar()
+city_text = str()
 city_entry = Entry(left_frame, textvariable=city_text)
 city_entry.grid(row=1, column=1, padx=5, pady=5)
 
 name_label = Label(left_frame, text="Name:")
 name_label.grid(row=2, column=0, padx=5, pady=5)
 
-name_text = StringVar()
+name_text = StringVar(root)
 name_entry = Entry(left_frame, textvariable=name_text)
 name_entry.grid(row=2, column=1, padx=5, pady=5)
 
-add_colleague_button = Button(left_frame, text="Add", command=add_colleague_label)
+add_colleague_button = Button(left_frame, text="Add", command=add_colleague_to_dict)
 add_colleague_button.grid(row=3, column=0, padx=5, pady=5, columnspan=2)
+
+confirmation_label = Label(left_frame, text="")
+confirmation_label.grid(row=4, column=0, padx=5, pady=5, columnspan=2)
 
 
 
