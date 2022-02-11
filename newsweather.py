a909f717fb99458c9cccd5ee2c5dec7e8d113d52
Shug1876 <2549100C@student.gla.ac.uk>
2022-02-11 20:09:42 +0000

    added news and weather functions

--- newsweather.py
+++ newsweather.py
@@ -1,5 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
+def weather(country, countries=""):
+    if len(countries)==0:
+        pass
+    pass
+
+def news(country, countries=""):
+    if len(countries)==0:
+        pass
+    pass
+
 search="Weather in the New York"
 URL ="https://openweathermap.org/city/5128581"
