8c0a26c4b72c0edec4ab45e742c8907c5e229fb8
2552805d <2552805d@student.gla.ac.uk>
2022-02-11 19:06:32 +0000

    Added city_entry

--- tinker_gui.py
+++ tinker_gui.py
@@ -1,3 +1,25 @@
 from tkinter import *
 
+
+def add_colleague_to_dict(name, city):
+    pass
+
+
+# ---- Create Root Window
 root = Tk()
+root.title("Remote Colleagues' Environment")
+root.geometry('700x350')
+
+# ---- Input college city and name
+city_label = Label(root, text="Enter City")
+city_label.pack()
+
+city_text = StringVar()
+city_entry = Entry(root, textvariable=city_text)
+city_entry.pack()
+
+colleagues = []
+
+
+
+root.mainloop()
