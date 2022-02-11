d937307730a6c24ad98c4787b9ccab4a7aea2f5e
Shug1876 <2549100C@student.gla.ac.uk>
2022-02-11 23:03:09 +0000

    more edits to weather function

--- newsweather.py
+++ newsweather.py
@@ -1,23 +1,42 @@
-#import requests
-from urllib.request import urlopen
+from urllib.request import urlopen, Request
 
-def weather(country, uk=""):
+def weather(country, location=""):
+    if type(country) != str or type(location) != str:
+        return "Parameter(s) entered is not a string. Please enter a string"
     country = country.title()
-    print("Weather in " + country)
+    string = "Weather in " + country
+    if len(location)!=0:
+        location = location.title()
+        string += ", "+location
+    print(string)
     country = country.lower()
-    if len(uk)!=0:
-        return "uk"
-    else:
-        if country=="usa" or country=="united states" or country == "united states of america":
-            country = "united_states_of_america"
+    if country=="uk" or country=="united kingdom":
+        country = "uk"
+    elif country=="usa" or country=="united states" or country == "united states of america":
+        country = "united_states_of_america"
+    
+    if country=="uk":
+        url = "https://www.metoffice.gov.uk/weather/forecast/uk"
+    elif len(location)!=0:
         url = "https://www.metoffice.gov.uk/weather/world/"+ country +"/list"
-        print(url)
-        page = urlopen(url)
+    else:
+        url = "https://www.metoffice.gov.uk/weather/world/"+ country
+    req = Request(url, headers={'User-Agent':'Mozilla/5.0'})
+    print(url)
+    page = urlopen(req).read().decode("utf-8")
+    if len(location)!= 0:
+        location_index = page.find(location)
+        print(location_index)
+        loc_start_index = location_index + len(location)
+        print(loc_start_index)
     return "passed"
 
 def news(country):
-    output1 = "Recent news in " + country
+    if type(country) != str:
+        return "Parameter entered is not a string. Please enter a string"
+    country = country.title()
+    print("Recent news in " + country)
 
-    return output1
-search="Weather in the New York"
-URL ="https://openweathermap.org/city/5128581"
+    return
+
+weather("usa", "detroit")
