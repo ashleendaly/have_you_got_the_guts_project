691049676a34381204d07b00eefdbb15273f18cd
2552805d <2552805d@student.gla.ac.uk>
2022-02-12 14:02:14 +0000

    fixed confirmation_label

--- tinker_gui.py
+++ tinker_gui.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from urllib.request import urlopen
 from country_list import countries_for_language
-from newsweather import news, weather
+# from newsweather import news, weather
 
 colleagues = []
 
@@ -9,7 +9,16 @@ countries = []
 for i in range(len(countries_for_language('en'))):
     countries += [countries_for_language('en')[i][1]]
 
-def add_colleague_to_dict():
+
+def add_colleague_to_list():
+
+    global country_entry
+    global name_entry
+    namestr = name_entry.get()
+    countrystr = country_entry.get()
+
+    
+    confirmation_label.config(text=f"{namestr} has been added")
     pass
 
 
@@ -28,26 +37,27 @@ right_frame.grid(row=0, column=1, padx=10, pady=5)
 # ---- Add Colleague
 add_colleague_label = Label(left_frame, text="Add Colleague").grid(row=0, column=0, padx=5, pady=5, columnspan=2)
 
-country_label = Label(left_frame, text="Country:")
-country_label.grid(row=1, column=0, padx=5, pady=5)
-
-country_text = StringVar()
-country_entry = Entry(left_frame, textvariable=country_text)
-country_entry.grid(row=1, column=1, padx=5, pady=5)
-
 name_label = Label(left_frame, text="Name:")
-name_label.grid(row=2, column=0, padx=5, pady=5)
+name_label.grid(row=1, column=0, padx=5, pady=5)
 
-name_text = StringVar()
-name_entry = Entry(left_frame, textvariable=name_text)
-name_entry.grid(row=2, column=1, padx=5, pady=5)
+name_entry = Entry(left_frame)
+name_entry.focus_set()
+name_entry.grid(row=1, column=1, padx=5, pady=5)
 
-add_colleague_button = Button(left_frame, text="Add", command=add_colleague_to_dict)
+country_label = Label(left_frame, text="Country:")
+country_label.grid(row=2, column=0, padx=5, pady=5)
+
+country_entry = Entry(left_frame)
+country_entry.focus_set()
+country_entry.grid(row=2, column=1, padx=5, pady=5)
+
+add_colleague_button = Button(left_frame, text="Add", command=add_colleague_to_list)
 add_colleague_button.grid(row=3, column=0, padx=5, pady=5, columnspan=2)
 
 confirmation_label = Label(left_frame, text="")
 confirmation_label.grid(row=4, column=0, padx=5, pady=5, columnspan=2)
 
+# ---- Display News and Weather
 
 
 
