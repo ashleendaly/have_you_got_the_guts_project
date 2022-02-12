79086d261dcd6389cc53b7b19572019a89ba5080
Shug1876 <2549100C@student.gla.ac.uk>
2022-02-12 13:47:38 +0000

    weather function now uses weather-forecast.com

--- newsweather.py
+++ newsweather.py
@@ -1,6 +1,9 @@
 from urllib.request import urlopen, Request
 from country_list import countries_for_language
+import string
 import mechanicalsoup as ms
+
+upr_alpha = list(string.ascii_uppercase)
 
 lwrcase_countries = []
 for i in range(len(countries_for_language('en'))):
@@ -8,7 +11,7 @@ for i in range(len(countries_for_language('en'))):
 
 browser = ms.Browser()
 
-# takes country name adn exact location and 
+# takes country name and exact location and returns links for weather via metoffice
 def weather(country, location):
     if type(country) != str or type(location) != str or len(country)==0 or len(location)==0:
         return "Invalid parameter(s) for program. Please enter valid parameters"
@@ -18,27 +21,34 @@ def weather(country, location):
     string += ", "+location
     print(string)
     country = country.lower()
+    url = 'https://www.weather-forecast.com/'
     if country=="uk" or country=="united kingdom":
-        country = "uk"
+        country = "united kingdom"
     elif country=="usa" or country=="united states" or country == "united states of america":
-        country = "united_states_of_america"
-    
-    if country=="uk":
-        url = "https://www.metoffice.gov.uk/weather/forecast/uk"
-    else:
-        url = "https://www.metoffice.gov.uk/weather/world/"+ country +"/list"
+        country = "united states"
+    country_url = country.replace(" ", "-")
+    country_url = "countries/"+country_url
+    url = url+country_url
+    def find_link(url, location):
         location = location.lower()
-    req = Request(url, headers={'User-Agent':'Mozilla/5.0'})
-    log_page = browser.get(url)
-    log_html = log_page.soup
-    links = log_html.select('a')
-    for link in links:
-        address = link['href']
-        text = link.text
-        text = text.strip().lower()
-        if text == location:
-            address = "https://www.metoffice.gov.uk" + address
-            return address
+        req = Request(url, headers={'User-Agent':'Mozilla/5.0'})
+        log_page = browser.get(url)
+        log_html = log_page.soup
+        links = log_html.select('a')
+        for link in links:
+            address = link['href']
+            text = link.text
+            text = text.strip().lower()
+            if text == location:
+                address = "https://www.weather-forecast.com/" + address
+                return address
+    address = find_link(url, location)
+    if address is None:
+        url = url + "/locations/" + location[0].upper()
+        address = find_link(url, location)
+        return address
+    else:
+        return address
 
 # takes country and returns link from bbc which contains data on the recent news
 def news(country):
@@ -78,7 +88,7 @@ def news(country):
                     address = "https://www.bbc.co.uk"+address
                     return address
 
-print(news('asia'))
+#print(news('asia'))
 
-s = weather("luxembourg", "ettelbruck")
+s = weather("uk", "wrexham")
 print(s)
