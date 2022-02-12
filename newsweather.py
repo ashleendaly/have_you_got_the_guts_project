0cc7e2a5cb027019cdc2e9ddf457f22772f1cf7f
Shug1876 <2549100C@student.gla.ac.uk>
2022-02-12 01:23:11 +0000

    news method returns address

--- newsweather.py
+++ newsweather.py
@@ -2,21 +2,19 @@ from urllib.request import urlopen, Request
 from country_list import countries_for_language
 import mechanicalsoup as ms
 
-print(countries_for_language('en')[0][1])
-
 lwrcase_countries = []
 for i in range(len(countries_for_language('en'))):
     lwrcase_countries += [countries_for_language('en')[i][1]]
 
+browser = ms.Browser()
+
 def weather(country, location):
-    browser = ms.Browser()
     if type(country) != str or type(location) != str or len(country)==0 or len(location)==0:
-        return "Invalid parameter(s) for program. Please enter a string"
+        return "Invalid parameter(s) for program. Please enter valid parameters"
     country = country.title()
     string = "Weather in " + country
-    if len(location)!=0:
-        location = location.title()
-        string += ", "+location
+    location = location.title()
+    string += ", "+location
     print(string)
     country = country.lower()
     if country=="uk" or country=="united kingdom":
@@ -30,7 +28,6 @@ def weather(country, location):
         url = "https://www.metoffice.gov.uk/weather/world/"+ country +"/list"
         location = location.lower()
     req = Request(url, headers={'User-Agent':'Mozilla/5.0'})
-    print(url)
     log_page = browser.get(url)
     log_html = log_page.soup
     links = log_html.select('a')
@@ -44,16 +41,42 @@ def weather(country, location):
 
 def news(country):
     if type(country) != str or len(country)==0:
-        return "Parameter entered is not a string. Please enter a string"
+        return "Invalid parameter for program. Please enter valid parameters"
     country = country.title()
     print("Recent news in " + country)
     country = country.lower()
-    if country=="uk" or country=="united kingdom":
-        url = "https://www.bbc.co.uk/news/uk"
-    elif country=="scotland" or country=="england" or country=="wales":
-        url = "https://www.bbc.co.uk/news/"+country
-    elif country == "northern ireland":
-        url = "https://www.bbc.co.uk/news/northern_ireland"
-    return
+    home_headers = ['england', 'n. ireland', 'scotland', 'wales']
+    if country in home_headers:
+        url = "https://www.bbc.co.uk/news"
+        req = Request(url, headers={'User-Agent':'Mozilla/5.0'})
+        log_page = browser.get(url)
+        log_html = log_page.soup
+        links = log_html.select('a')
+        for link in links:
+            address = link['href']
+            text = link.text
+            text = text.strip().lower()
+            if text in country:
+                print(text+" "+address)
+    else:
+        if country == 'usa':
+            country = 'us '
+        world_headers = ['africa', 'asia', 'australia', 'europe', 'latin america', 'middle east', 'us & canada']
+        url = "https://www.bbc.co.uk/news/world"
+        req = Request(url, headers={'User-Agent':'Mozilla/5.0'})
+        log_page = browser.get(url)
+        log_html = log_page.soup
+        links = log_html.select('a')
+        for link in links:
+            address = link['href']
+            text = link.text
+            text = text.strip().lower()
+            if text in world_headers:
+                if country in text:
+                    address = "https://www.bbc.co.uk/"+address
+                    return address
 
-weather("usa", "detroit")
+#print(news('australia'))
+
+#s = weather("usa", "detroit")
+#print(s)
