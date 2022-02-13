72a845647a48252966712d6a816704215b416489
LolzerSpock <86776369+LolzerSpock@users.noreply.github.com>
2022-02-13 10:35:36 +0000

    Additional changes to display

--- tinker_gui.py
+++ tinker_gui.py
@@ -1,5 +1,6 @@
 import os
 from tkinter import *
+from tkinter import messagebox
 from urllib.request import urlopen
 from newsweather import weather, news
 from PIL import Image, ImageTk
@@ -30,15 +31,21 @@ def remove_colleague_to_list():
 # ---- Create Root Window
 root = Tk()
 root.title("Remote Colleagues' Environment")
-root.maxsize(900, 600)
+root.geometry('900x500')
 
 # ---- Create left and right frames
 
 left_frame = Frame(root, width=200, height=400)
 left_frame.grid(row=0, column=0,  padx=20, pady=5)
 
-right_frame = Frame(root, width=650, height=400)
+right_frame = Frame(root, width=650, height=500)
 right_frame.grid(row=0, column=1, padx=10, pady=5)
+
+#---- Images ----
+im = Label(root, bitmap="")
+im.grid(row=0, column=1, pady=5, columnspan=2)
+
+im['bitmap'] = crt_path+"/Earth.png"
 
 # ------- LEFT FRAME ----------
 
@@ -56,15 +63,15 @@ label = Label(
 label.place(x=40, y=15)
 
 # ---- Add Colleague
-add_colleague_label = Label(left_frame, text="Add Colleague").grid(row=0, column=0, pady=5, columnspan=2)
+add_colleague_label = Label(left_frame, text="Add Colleague").grid(row=1, column=0, pady=5, columnspan=2)
 
 # ---- name information ----
 name_label = Label(left_frame, text="Name:")
-name_label.grid(row=1, column=0, pady=5)
+name_label.grid(row=2, column=0, pady=5)
 
 name_entry = Entry(left_frame)
 name_entry.focus_set()
-name_entry.grid(row=1, column=1, pady=5)
+name_entry.grid(row=2, column=1, pady=5)
 
 # ---- city information ----
 def on_click_cit(event):
@@ -74,16 +81,16 @@ def on_click_cit(event):
     # make the callback only work once
     city_entry.unbind('<Button-1>', on_click_city)
 
-#city_label = Label(left_frame, text="City:")
-#city_label.grid(row=2, column=0, pady=5)
+city_label = Label(left_frame, text="City:")
+city_label.grid(row=3, column=0, pady=5)
 
-location_label = Label(left_frame, text="Location:")
-location_label.grid(row=2, column=0, pady=5)
+#location_label = Label(left_frame, text="Location:")
+#location_label.grid(row=2, column=0, pady=5)
 
 city_entry = Entry(left_frame)
 city_entry.insert(0, "Insert City...")
 city_entry.focus_set()
-city_entry.grid(row=2, column=1, padx=5, pady=5)
+city_entry.grid(row=3, column=1, padx=5, pady=5)
 
 on_click_city = city_entry.bind('<Button-1>', on_click_cit)
 
@@ -95,46 +102,65 @@ def on_click_count(event):
     # make the callback only work once
     country_entry.unbind('<Button-1>', on_click_country)
 
-#country_label = Label(left_frame, text="Country:")
-#country_label.grid(row=3, column=0, pady=5)
+country_label = Label(left_frame, text="Country:")
+country_label.grid(row=4, column=0, pady=5)
 
 country_entry = Entry(left_frame)
 country_entry.insert(0, "Insert Country...")
 country_entry.focus_set()
-country_entry.grid(row=3, column=1, padx=5, pady=5)
+country_entry.grid(row=4, column=1, padx=5, pady=5)
 
 on_click_country = country_entry.bind('<Button-1>', on_click_count)
 
 # ---- add button ----
 add_colleague_button = Button(left_frame, text="Add", command=add_colleague_to_list)
-add_colleague_button.grid(row=4, column=0, pady=5, columnspan=2)
+add_colleague_button.grid(row=5, column=0, pady=5, columnspan=2)
 
 # ---- confirmation ----
 confirmation_label = Label(left_frame, text="")
-confirmation_label.grid(row=5, column=0, pady=5, columnspan=2)
+confirmation_label.grid(row=6, column=0, pady=5, columnspan=2)
 
 
 # ---- Select Colleague Drop Down Menu ----
 
 existing_colleague_label = Label(left_frame, text="Select Existing Colleague")
-existing_colleague_label.grid(row=6, column=0, pady=5, columnspan=2)
+existing_colleague_label.grid(row=7, column=0, pady=5, columnspan=2)
 
 clicked = StringVar()
 clicked.set("Select...") # default value
 
-colleague_drop = OptionMenu(left_frame, clicked, colleagues)
-colleague_drop.grid(row=7, column=0, pady=5, columnspan=2)
+#colleagues
+
+def clear_label_image():
+        im.destroy()
+
+def callback(*choices):
+    clear_label_image()
+
+    #space = Label(left_frame, text="")
+    #space.grid(row=0, column=0, pady=0)
+
+    basic_font_color = "#ccc4c4"
+    bg_color_weather = "#00adad"
+    bg_color_news = "#a6a8a6"
+
+    Weather_Display = Label(right_frame, text="", font=("times new roman", 30, "bold"), bg=bg_color_weather,
+        fg=basic_font_color, bd=10, relief=GROOVE)
+    Weather_Display.place(x=80, y=10, width=480, relheight=0.4)
+
+    News_Display = Label(right_frame, text="", font=("times new roman", 30, "bold"), bg=bg_color_news,
+        fg=basic_font_color, bd=10, relief=GROOVE)
+    News_Display.place(x=80, y=250, width=480, relheight=0.4)
+
+
+
+choices = ['op1', 'op2']
+
+
+colleague_drop = OptionMenu(left_frame, clicked, *choices, command=callback)
+colleague_drop.grid(row=8, column=0, pady=5, columnspan=2)
 
 # ------- Right FRAME ----------
-
-#====Images====#
-image = Label(root, bitmap="")
-image.grid(row=0, column=1, pady=5, columnspan=2)
-
-image['bitmap'] = crt_path+"/Earth.png"
-
-
-
 # ---- Display Weather
 
 
@@ -143,6 +169,11 @@ image['bitmap'] = crt_path+"/Earth.png"
 
 
 
+def on_closing():
+    if messagebox.askokcancel("Quit", "Would you like to exit the programme?"):
+        root.destroy()
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop()
 
 
